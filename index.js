2db47f60df253c728508f7bdcc5a03e379ddbf7d
Personal <anastasiabielievitina@AnabelMac.lan>
2023-07-12 11:20:47 -0700

    improve code clarity

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/stylesheets"));
 app.use(express.static(__dirname + "/js"));
 
-const DB = process.env.DB.replace("<password>", process.env.DB_PASS);
+const DB = process.env.DB;
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
